bbf8bac6cf13b6b2da590d7c27b2f31690cccbef
Nigade <2317276@cognizant.com>
2024-04-11 16:28:01 +0530

    Controller signup code added for trainee and trainer by siddhant on 11/4

--- src/main/java/com/SpringBoot1/SpringBoot_1/ControllerSignUp.java
+++ src/main/java/com/SpringBoot1/SpringBoot_1/ControllerSignUp.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.Spring.Boot1.Model.Logins;
+
 @Controller
 public class ControllerSignUp {
 	
@@ -23,6 +25,12 @@ public class ControllerSignUp {
 //		SignUp sign_up = new SignUp();
 //		check = sign_up.trainee_signup(first_name,last_name,dob,feild_of_study,email,password)
 		if(check) {
+			mv.addObject("first_name",first_name);
+			mv.addObject("last_name",last_name);
+			mv.addObject("dob",dob);
+			mv.addObject("feild_of_study",feild_of_study);
+			mv.addObject("email",email);
+			mv.addObject("password",password);
 			mv.setViewName("trainee");
 		}else {
 			mv.setViewName("login");
@@ -45,7 +53,15 @@ public class ControllerSignUp {
 //		SignUp sign_up = new SignUp();
 //		check = sign_up.trainer_signup(first_name,last_name,DOB,expertise,email,password)
 		if(check) {
+//			below 6 lines are added by siddhant nigade on 11/4/2024
+			mv.addObject("first_name",first_name);
+			mv.addObject("last_name",last_name);
+			mv.addObject("DOB",DOB);
+			mv.addObject("expertise",expertise);
+			mv.addObject("email",email);
+			mv.addObject("password",password);
 			mv.setViewName("trainer");
+			
 		}else {
 			mv.setViewName("login");
 		}
